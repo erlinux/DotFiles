0b5c4a42352c04539415a45fd6923c6d2b05cdcf
erlinux <jiwenkangatech@foxmail.com>
2017-07-20 21:01:56 +0800

    Update

--- shell/imp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PID=$(ps -ef |grep /etc/shadowsocks.json | awk '{print $2 }' | head -n1)
-netstat -antlp |grep 50011 1>/dev/null 2>&1
-if [ $? -eq 0 ] ; then
-	echo "PID:$PID Shadowsocks was running"
-else
-	setsid ssserver -c /etc/shadowsocks.json &
-fi
